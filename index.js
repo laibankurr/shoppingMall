67402fc40a3d4b23872e4dfcb114f5a7ae00cc12
veris1975 <veris1975@outlook.com>
2020-05-26 19:59:19 -0500

    add user register route

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const port = 5000;
 const mongoose = require("mongoose");
 const dbIdPw = require("./mongoDB_Info");
 const mongoDB_URI = `mongodb+srv://${dbIdPw.id}:${dbIdPw.pw}@cluster0-6htbs.mongodb.net/test?retryWrites=true&w=majority`;
+const bodyParser = require("body-parser");
+const { User } = require("./model/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose
   .connect(mongoDB_URI, {
@@ -16,4 +21,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Succeed to get from Home"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, res) => {
+    if (err) {
+      return res.json({ success: false, err });
+    } else {
+      return res.status(200).json({
+        success: true,
+      });
+    }
+  });
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
